01ab493e8646351bbc0279fff513b04b9e090fe3
tseringnam <35940410+tseringnam@users.noreply.github.com>
2019-05-14 00:52:06 -0400

    Update mod-ec2.tf

--- module/ec2-web/mod-ec2.tf
+++ module/ec2-web/mod-ec2.tf
@@ -4,5 +4,8 @@ resource "aws_instance" "myweb" {
    ami = "${var.ami}"
    instance_type = "${var.instance_type}"
    security_groups = ["default"]
+   tags {
+      Managed = "Terraform"
+      }
    
    }
